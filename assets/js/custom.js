fa5ae0993429cd2e8a70f7590cdca4e5b01b3645
Kirill <k@lis.im>
2021-06-02 12:20:41 +0300

    Tweaks with parallax effect

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,15 @@
-$('.bg-holder').parallaxScroll({
+$('.parallax-header').parallaxScroll({
   friction: 0.5
+});
+
+$('.parallax').parallaxScroll({
+  friction: 0.5
+});
+
+$('.parallax-footer').parallaxScroll({
+  friction: 0.5
+});
+
+$('.parallax-single').parallaxScroll({
+  friction: 0.2
 });
