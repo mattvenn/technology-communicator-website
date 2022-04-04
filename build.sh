f378490591eb3678e1d1ccc6fc4d85f9ab97cf9c
Matt Venn <matt@mattvenn.net>
2022-04-04 11:10:29 +0200

    update copyright and do rsync

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ HUGO_ENV=production hugo
 #git add *
 #git commit -m 'update'
 #git push
-tar -czf sci-com.tar.gz public
-scp sci-com.tar.gz matt@zerotoasiccourse.com:/tmp/
+rsync -avzh public matt@zerotoasiccourse.com:/home/matt/sci-com
+
